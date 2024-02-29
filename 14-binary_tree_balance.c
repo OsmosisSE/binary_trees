90b5e118249c0f739407f054e0dcc03c1522e621
OsmosisSE <sirosmosis1@gmail.com>
2024-02-29 13:30:38 -0800

    binary tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,13 +7,22 @@
  */
 size_t binary_tree_height_b(const binary_tree_t *tree)
 {
+	size_t left_height = 0;
+	size_t right_height = 0;
+
 	if (tree == NULL)
+	{
 		return (0);
-
-	size_t left_height = tree->left ? 1 + binary_tree_height_b(tree->left) : 0;
-	size_t right_height = tree->right ? 1 + binary_tree_height_b(tree->right) : 0;
-
-	return (left_height > right_height ? left_height : right_height);
+	}
+	else
+	{
+		if (tree)
+		{
+			left_height = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
+			right_height = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+		}
+		return ((left_height > right_height) ? left_height : right_height);
+	}
 }
 
 /**
@@ -23,11 +32,13 @@ size_t binary_tree_height_b(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	int right_height = 0, left_height = 0, total = 0;
 
-	int left_height = (int)binary_tree_height_b(tree->left);
-	int right_height = (int)binary_tree_height_b(tree->right);
-
-	return (left_height - right_height);
+	if (tree)
+	{
+		left_height = (int)binary_tree_height_b(tree->left);
+		right_height = (int)binary_tree_height_b(tree->right);
+		total = left_height - right_height;
+	}
+	return (total);
 }
